4b82297a25b002155b34ff504479ca90a87f6933
Jake Soenneker <jake@soenneker.com>
2025-03-26 12:22:24 -0500

    Additional values

--- src/CronPresets.cs
+++ src/CronPresets.cs
@@ -31,15 +31,38 @@ public static class CronPresets
     /// <summary> Every 6 hours </summary>
     public const string Every6Hours = "0 */6 * * *";
 
+    /// <summary> Every 12 hours </summary>
+    public const string Every12Hours = "0 */12 * * *";
+
+    // ----- Anchors -----
+
+    /// <summary> Start of every hour </summary>
+    public const string StartOfHour = "0 * * * *";
+
+    /// <summary> Start of every day (midnight) </summary>
+    public const string StartOfDay = "0 0 * * *";
+
+    /// <summary> Start of every week (Sunday at midnight) </summary>
+    public const string StartOfWeek = "0 0 * * SUN";
+
+    /// <summary> Start of every month (1st at midnight) </summary>
+    public const string StartOfMonth = "0 0 1 * *";
+
+    /// <summary> Start of every year (January 1st at midnight) </summary>
+    public const string StartOfYear = "0 0 1 1 *";
+
     // ----- Daily -----
 
     public const string DailyMidnight = "0 0 * * *";
     public const string DailyNoon = "0 12 * * *";
+    public const string Daily2AM = "0 2 * * *";
+    public const string Daily4AM = "0 4 * * *";
     public const string Daily6AM = "0 6 * * *";
     public const string Daily8AM = "0 8 * * *";
     public const string Daily9AM = "0 9 * * *";
     public const string Daily5PM = "0 17 * * *";
     public const string Daily8PM = "0 20 * * *";
+    public const string Daily10PM = "0 22 * * *";
 
     // ----- Weekday/Weekend -----
 
